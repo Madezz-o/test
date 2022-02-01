6e4bec17123691d7261754b82a735837f03d94f2
Madezz-o <svpomadezo@gmail.com>
2022-02-01 11:36:54 +0100

    test division et division par Zero

--- test/CalculsTest.java
+++ test/CalculsTest.java
@@ -41,6 +41,15 @@ class CalculsTest {
         );
     }
     
+    static Stream<Arguments> chargerJeuDeTestDivision() throws Throwable 
+    {
+        return Stream.of(
+            Arguments.of(2,2,1),
+            Arguments.of(21,7,3),
+            Arguments.of(6,2,3)
+        );
+    }
+    
     
     
 	@ParameterizedTest(name="Additionner numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
@@ -67,6 +76,23 @@ class CalculsTest {
 	{
 	        Calculs monCal = new Calculs(firstNumber, secondNumber);
 	        assertEquals(expectedResult, monCal.multiplier(), "test en échec pour " + firstNumber + " * " + secondNumber + " != " + expectedResult); 
+	}
+	
+	
+	@ParameterizedTest(name="Division numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+	@MethodSource("chargerJeuDeTestDivision")
+	void testDiviser(int firstNumber, int secondNumber, int expectedResult) 
+	{
+			Calculs monCal = new Calculs(firstNumber, secondNumber);
+	        assertEquals(expectedResult, monCal.diviser(), "test en échec pour " + firstNumber + " / " + secondNumber + " != " + expectedResult); 	
+	}
+	
+	void testDiviserParZero() 
+	{
+			Calculs monCal = new Calculs(50, 0);
+	        assertThrows(ArithmeticException.class, () -> {
+	        	monCal.diviser();
+	        }); 	
 	}
 
 }
